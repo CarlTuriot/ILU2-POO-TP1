bd702b93bd2646eed423123a0065218f0a4dfe15
Carl Turiot <trc4585a@ensfsi.onmicrosoft.com>
2026-02-25 11:00:44 +0100

    trouverVendeur finished

--- src/villagegaulois/Marche.java
+++ src/villagegaulois/Marche.java
@@ -1,5 +1,54 @@
 package villagegaulois;
 
-public class Marche {
+import personnages.Gaulois;
 
+public class Marche {
+	private int nombreEtals;
+	private Etal[] etals;
+	
+	public Marche(int nombreEtal) {
+		this.nombreEtals = nombreEtals;
+		etals = new Etal[nombreEtals];
+	}
+	
+	void utiliserEtal(int indiceEtal, Gaulois vendeur,String produit, int nbProduit) {
+		etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+	}
+	
+	int trouverEtalLibre() {
+		for(int i=0;i<nombreEtals;i++) {
+			if (!etals[i].isEtalOccupe())return i;
+		}
+		return -1;
+	}
+	
+	//modification certaines
+	Etal[] trouverEtals(String produit) {
+		int nbEtalValid=0;
+		for(int i=0;i<nombreEtals;i++) {
+			if (etals[i].contientProduit(produit)) {
+				nbEtalValid++;
+			}
+		}
+		Etal[] etalsValid= new Etal[nbEtalValid];
+		int indiceEtal=0;
+		
+		for(int j=0;j<nombreEtals;j++) {
+			if (etals[j].contientProduit(produit)) {
+				etalsValid[indiceEtal]=etals[j];
+				indiceEtal++;
+			}
+		}
+		return etalsValid;
+	}
+	
+	Etal trouverVendeur(Gaulois gaulois) {
+		for(int i=0;i<nombreEtals;i++) {
+			if (gaulois.equals(etals[i].getVendeur()))return etals[i];
+		}
+		return null;
+	}
+	
+	
+	
 }
