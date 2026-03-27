11df4ea6aa4b468a8e1278619a3287e8352f7212
CarlTuriot <carl.turiot@gmail.com>
2026-03-27 12:46:21 +0100

    finished

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -107,4 +107,73 @@ public class Village {
 		return marche.afficherMarche();
 	}
 	
+	public class Marche {
+		private int nombreEtal;
+		private Etal[] etals;
+		
+		public Marche(int nombreEtal) {
+			this.nombreEtal = nombreEtal;
+			etals = new Etal[nombreEtal];
+		}
+		
+		public void utiliserEtal(int indiceEtal, Gaulois vendeur,String produit, int nbProduit) {
+			if (etals[indiceEtal]==null) {
+				etals[indiceEtal]=new Etal();
+			}
+			etals[indiceEtal].occuperEtal(vendeur, produit, nbProduit);
+		}
+		
+		public int trouverEtalLibre() {
+			for(int i=0;i<nombreEtal;i++) {
+				if (etals[i]==null || !etals[i].isEtalOccupe())return i;
+			}
+			return -1;
+		}
+		
+		public Etal[] trouverEtals(String produit) {
+			int nbEtalValid=0;
+			for(int i=0;i<nombreEtal;i++) {
+				if (etals[i]!=null && etals[i].isEtalOccupe() && etals[i].contientProduit(produit)) {
+						nbEtalValid++;
+					}
+			}
+			
+			Etal[] etalsValid= new Etal[nbEtalValid];
+			int indiceEtal=0;
+			
+			for(int j=0;j<nombreEtal;j++) {
+				if (etals[j]!=null && etals[j].isEtalOccupe() && etals[j].contientProduit(produit)) {
+						etalsValid[indiceEtal]=etals[j];
+						indiceEtal++;
+					}
+			}
+			return etalsValid;
+		}
+		
+		public Etal trouverVendeur(Gaulois gaulois) {
+			for(int i=0;i<nombreEtal;i++) {
+				if (gaulois.equals(etals[i].getVendeur()))return etals[i];
+			}
+			return null;
+		}
+		
+		public String afficherMarche(){
+			int nombreEtalVide=0;
+			StringBuilder etalsOccupe= new StringBuilder("Les etals du marche son les suivants:\n");
+			
+			for(int i=0;i<nombreEtal;i++) {
+				if (etals[i]!=null && etals[i].isEtalOccupe()) {
+					etalsOccupe.append(etals[i].afficherEtal());
+				} else {
+					nombreEtalVide++;
+				}
+			}
+			if(nombreEtalVide!=0) {
+				etalsOccupe.append("Il reste " + nombreEtalVide + " etals non utilises dans le marche.\n");
+			}
+			return etalsOccupe.toString();
+		}
+		
+	}
 }
+
